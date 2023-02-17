cd7374c2baa61d73332ab12798746f71054075b4
Simeon Nzube Dominic <simeondominic@gmail.com>
2023-02-17 13:33:47 +0000

    C variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	for
+	(b = "a";
+	 b <= "z";
+	 b++);
+	{
+		putchar(b);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
