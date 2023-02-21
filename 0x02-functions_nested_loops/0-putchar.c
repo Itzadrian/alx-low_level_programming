5afae069861ec2061c7d721f4cb14e754b80cb39
Simeon Nzube Dominic <simeondominic@gmail.com>
2023-02-21 21:23:29 +0000

    C functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
-
-	for (i = 0; i < 9; i++)
-	{
-	_putchar(p[i]);
-	}
+	char _putchar(char c)
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 	return (0);
 }
