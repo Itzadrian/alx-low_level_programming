d8f885be5e4cb08647f218b1d657a340baadf8d7
Simeon Nzube Dominic <simeondominic@gmail.com>
2023-03-23 19:21:41 +0000

    C variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct typ - Struct operators
+ *
+ * @c: Char
+ * @tp: The function associated
+ */
+typedef struct typ
+{
+	char *c;
+	void (*tp)(va_list var);
+} typ_t;
+
 #endif
